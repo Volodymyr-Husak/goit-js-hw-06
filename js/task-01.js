faaf4af852b5aba89a4fd53cda08dd8b9d2ee362
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-07 14:00:32 +0300

    finishes task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,41 +1,9 @@
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
-// const categoriesArrayEl = document.querySelectorAll('.item');
-// console.log(categoriesArrayEl);
-
-// const categoriesNumber = categoriesArrayEl.length;
-// console.log(`Number of categories: ${categoriesNumber}`);
-// console.log('')
-
-// const firstCategoriesText = document.querySelector('h2').textContent;
-// console.log(`Category: ${firstCategoriesText}`);
-// const firstCategoriesNumber = document.querySelectorAll('.item').length;
-
-// console.log(document.querySelectorAll('.item'));
-// console.log(`Elements: ${firstCategoriesNumber}`);
-
-// const secondCategoriesText = document.querySelector('h2').textContent;
-// const list = body.firstElementChild;
-// console.log(list);
-//
-// elem.firstElementChild
-// const ul = document.querySelector;
-// =============================================================================================================
-console.log('===========================================================')
+console.log('=====================Option #1======================');
 const categoriesArrayEl = document.querySelectorAll('.item');
 // console.log(categoriesArrayEl);
 const categoriesNumber = categoriesArrayEl.length;
 console.log(`Number of categories: ${categoriesNumber}`);
-console.log('')
+console.log('');
 
 const categoriesEl = document.querySelector('#categories');
 // console.log(categoriesEl);
@@ -49,8 +17,47 @@ console.log(`Category: ${animalsTitle}`);
 
 const animalsNumber = animalsItemEl.querySelectorAll('li').length;
 console.log(`Elements: ${animalsNumber}`);
+console.log('');
+// ===============================Products======================================
+const productsItemEl = categoriesEl.children[1];
+
+const productsTitle = productsItemEl.querySelector('h2').textContent;
+console.log(`Category: ${productsTitle}`);
+
+const productsNumber = productsItemEl.querySelectorAll('li').length;
+console.log(`Elements: ${productsNumber}`);
+console.log('');
+// ===============================Technologies======================================
+const technologiesItemEl = categoriesEl.children[2];
+
+const technologiesTitle = technologiesItemEl.querySelector('h2').textContent;
+console.log(`Category: ${technologiesTitle}`);
+
+const technologiesNumber = technologiesItemEl.querySelectorAll('li').length;
+console.log(`Elements: ${technologiesNumber}`);
 
 
+// ===============================V2======================================
+console.log('=====================Option #2======================');
 
+const categoriesArrayElV2 = document.querySelectorAll('.item');
 
+const categoriesNumberV2 = categoriesArrayEl.length;
+console.log(`Number of categories: ${categoriesNumberV2}`);
+console.log('');
+
+const categoriesElV2 = document.querySelector('#categories');
+
+function categories (number) {
+    const categoriesItemEl = categoriesEl.children[number];
+    const categoriesTitle = categoriesItemEl.querySelector('h2').textContent;
+    console.log(`Category: ${categoriesTitle}`);
+    const categoriesNumber = categoriesItemEl.querySelectorAll('li').length;
+    console.log(`Elements: ${categoriesNumber}`);
+    console.log('');   
+};
+
+categories(0);
+categories(1);
+categories(2);
 
