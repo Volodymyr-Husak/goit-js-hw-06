d622d7674d4007365eca12bd3823f8b7b00b70a0
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-08 18:16:23 +0300

    Corrects task-05

--- js/task-05.js
+++ js/task-05.js
@@ -9,3 +9,15 @@ console.log(nameOutputEl);
 nameInputEl.addEventListener("input", (event) => {
     nameOutputEl.textContent = event.currentTarget.value;
 });
+
+nameInputEl.addEventListener("change", emptyStringCheck);
+
+function emptyStringCheck() {
+    const stringLength = nameInputEl.value.split('').length;
+
+    nameInputEl.addEventListener("blur", () => {
+        if (stringLength === 0) {
+            nameOutputEl.textContent = 'Anonymous';
+        } 
+    });
+};
