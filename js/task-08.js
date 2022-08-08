0a98393a11f3af6132bd1fb27a85b7f91e33e004
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-08 22:39:28 +0300

    Creates task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const emailValue = event.currentTarget.elements.email.value;
+    const passwordValue = event.currentTarget.elements.password.value;
+    
+    if (emailValue === "" || passwordValue === "") {
+        return alert("Please fill in all the fields!");
+    };
+    
+    const formObj = {
+        email: emailValue,
+        password: passwordValue,
+    };
+    console.log(formObj);
+    
+    formEl.reset();
+};
