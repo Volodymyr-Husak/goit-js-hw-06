c56bfd037830f33dd9f31eccef22af5fe6b74f3d
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-09 00:23:08 +0300

    Creates task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const buttonEl = document.querySelector('.change-color');
+
+const colorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
