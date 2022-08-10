729d8995609ffbbc75dc136c94d8591409cc2dac
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-10 21:47:37 +0300

    corrects task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,17 +2,16 @@ const inputEl = document.querySelector("#validation-input");
 
 console.log(inputEl);
 
-inputEl.addEventListener("change", checkCorrectLengthString);
+inputEl.addEventListener("input", checkCorrectLengthString);
 
 function checkCorrectLengthString() {
-    const stringLength = inputEl.value.split('').length;
+  const stringLength = inputEl.value.split("").length;
 
-    inputEl.addEventListener("blur", () => {
-        if (stringLength < 6) {
-            inputEl.classList = 'invalid'
-        }
-        else {
-            inputEl.classList = 'valid'
-        };
-    });
-};
+  inputEl.addEventListener("blur", () => {
+    if (stringLength < 6 || stringLength > 6) {
+      inputEl.classList = "invalid";
+    } else {
+      inputEl.classList = "valid";
+    }
+  });
+}
